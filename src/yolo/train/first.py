f44ff076508293f1891dc49980afbb3ea26d54ed
Sunjoo Park <all4dich@gmail.com>
2025-04-09 11:04:06 +0900

    yolo: Enable cleaml logging on Python training code.

--- src/yolo/train/first.py
+++ src/yolo/train/first.py
@@ -1,19 +1,36 @@
 from pathlib import Path
 from ultralytics import YOLO
-from ultralytics.utils import SETTINGS
+from ultralytics import settings
+from clearml import Task
+from datetime import datetime
+#from ultralytics.utils import SETTINGS
 import os
-yolo_path_str = f"{os.environ['HOME']}/.local/yolo"
-yolo_path = Path(f"{yolo_path_str}")
-yolo_path.mkdir(parents=True, exist_ok=True)
 
-runs_path_str = f"{yolo_path_str}/runs"
-runs_path = Path(f"{runs_path_str}")
-runs_path.mkdir(parents=True, exist_ok=True)
+print(settings)
 
-SETTINGS['weights_dir'] = f"{yolo_path_str}/weights"
-SETTINGS['datasets_dir'] = f"{yolo_path_str}/datasets"
-SETTINGS['runs_dir'] = runs_path_str
+#yolo_path_str = f"{os.environ['HOME']}/.local/yolo"
+#yolo_path = Path(f"{yolo_path_str}")
+#yolo_path.mkdir(parents=True, exist_ok=True)
+#
+#runs_path_str = f"{yolo_path_str}/runs"
+#runs_path = Path(f"{runs_path_str}")
+#runs_path.mkdir(parents=True, exist_ok=True)
 
-model = YOLO("yolo11n.pt")
+#SETTINGS['weights_dir'] = f"{yolo_path_str}/weights"
+#SETTINGS['datasets_dir'] = f"{yolo_path_str}/datasets"
+#SETTINGS['runs_dir'] = runs_path_str
+
+#settings.update({'weights_dir': f"{yolo_path_str}/weights",
+#                 'datasets_dir': f"{yolo_path_str}/datasets",
+#                 'runs_dir': runs_path_str})
+model_path = "yolo11n.pt"
+model = YOLO(model_path)
+task = Task.init(project_name="Shoreline Park", task_name=f"yolo11n-{str(datetime.now())}", tags=["yolo11n", "test_task", model_path])
+task.add_tags("coco8.yaml")
+task.set_parameter("model_variant", "yolo11n")
 # Train the model
-results = model.train(data="coco8.yaml", epochs=1, imgsz=640)
+args = dict(data="coco8.yaml", epochs=10, device="cpu")
+task.connect(args)
+results = model.train(**args)
+model.eval()
+
